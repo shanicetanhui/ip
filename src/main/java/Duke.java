52ca2b6324f547352f8f2122d49abda29d85bff6
Shanice <shanicetanhui1212@gmail.com>
2025-01-26 22:19:17 +0800

    Level-0. Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,22 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo = " ____            _         \n"
+                    + "|  __ \\         | |  ___    \n"
+                    + "| |__) |___  ___| |_/ _ \\ \n"
+                    + "|  ___/ _ \\/ __| __| | | |\n"
+                    + "| |  |  __/\\__ \\ |_| |_| |\n"
+                    + "|_|   \\___||___/\\__|\\__,_|\n";
         System.out.println("Hello from\n" + logo);
+
+        // Greeting message
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm Pesto!");
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        // Exit message
+        System.out.println("____________________________________________________________");
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
     }
 }
